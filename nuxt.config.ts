343dbf5fdfebd688e87ca842493c59402762350e
Dipesh Acharya <dipesh@awecode.com>
2025-06-25 14:00:35 +0545

    fix: add page route for list view and fix alias for layer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,20 @@ export default defineNuxtConfig({
       // Use the generated ESLint config for lint root project as well
       rootDir: fileURLToPath(new URL('..', import.meta.url))
     }
+  },
+  $meta: {
+    name: 'autoadmin',
+  },
+  alias: {
+    '#layers/automadmin': fileURLToPath(new URL('.', import.meta.url))
+  },
+  hooks: {
+    'pages:extend': function (pages) {
+      pages.push({
+        name: 'list',
+        path: '/:modelLabel(.+)/',
+        file: '#layers/automadmin/pages/list/index.vue',
+      })
+    }
   }
 })
