1ff20f55e52122cb298e58b58d6a94f0f9f79789
Dipesh Acharya <dipesh@awecode.com>
2025-07-15 11:30:22 +0545

    refactor: move icon map to string util file

--- pages/_autoadmin/index.vue
+++ pages/_autoadmin/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
+  <div class="container mx-auto px-4 py-8 max-w-none">
+    <div class="flex gap-4">
+      <div>
+        <Sidebar />
+      </div>
+      <div class="flex-1">
+      </div>
+    </div>
   </div>
 </template>
