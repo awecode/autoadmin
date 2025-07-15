464be23329798a8c9398c073e4b77a63b1ab8861
Dipesh Acharya <dipesh@awecode.com>
2025-07-15 13:52:07 +0545

    chore: improve sidebar style

--- components/Sidebar.vue
+++ components/Sidebar.vue
@@ -19,8 +19,8 @@ await callOnce(async () => {
 })
 
 const items = ref<NavigationMenuItem[][]>([
+  appConfig.sidebar.topItems,
   [
-    ...appConfig.sidebar.topItems,
     appConfig.sidebar.modelLabel,
     ...modelLinks.value,
   ],
@@ -29,23 +29,30 @@ const items = ref<NavigationMenuItem[][]>([
 </script>
 
 <template>
-  <div class="relative">
-    <UNavigationMenu
-      class="data-[orientation=vertical]:w-48"
-      orientation="vertical"
-      :class="{ 'data-[orientation=vertical]:w-12': collapsed }"
-      :collapsed="collapsed"
-      :items="items"
-    />
-
-    <!-- Collapse/Uncollapse button -->
-    <UButton
-      square
-      class="absolute top-4 right-4 z-10"
-      size="sm"
-      variant="ghost"
-      :icon="collapsed ? 'i-lucide-chevron-right' : 'i-lucide-chevron-left'"
-      @click="collapsed = !collapsed"
-    />
+  <div class="rounded-lg border border-gray-200 dark:border-gray-800 py-4" :class="{ 'px-4': !collapsed }">
+    <div class="flex justify-between items-center mb-4">
+      <div v-if="!collapsed" class="text-lg font-bold">
+        AutoAdmin
+      </div>
+      <UButton
+        color="neutral"
+        variant="ghost"
+        :class="{ 'mx-auto': collapsed }"
+        :icon="collapsed ? 'i-lucide-panel-left-open' : 'i-lucide-panel-left-close'"
+        :title="collapsed ? 'Expand' : 'Collapse'"
+        @click="collapsed = !collapsed"
+      />
+    </div>
+    <div>
+      <UNavigationMenu
+        popover
+        tooltip
+        class="data-[orientation=vertical]:w-48 gap-4"
+        orientation="vertical"
+        :class="{ 'data-[orientation=vertical]:w-12 items-center': collapsed }"
+        :collapsed="collapsed"
+        :items="items"
+      />
+    </div>
   </div>
 </template>
