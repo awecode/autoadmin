1fa795c7a12437e4441c93fd740391655e49aa58
Dipesh Acharya <dipesh@awecode.com>
2025-07-19 13:26:13 +0545

    feat: use tiptap editor for rich text

--- components/RichTextEditor.client.vue
+++ components/RichTextEditor.client.vue
@@ -1,16 +1,33 @@
 <script setup lang="ts">
+import StarterKit from '@tiptap/starter-kit'
+import { useEditor, EditorContent, Editor } from '@tiptap/vue-3'
+
 const props = defineProps<{
-  modelValue: string
-  attrs: Record<string, any>
+  modelValue?: string
+  attrs?: Record<string, any>
 }>()
 
-defineEmits<{
+const emit = defineEmits<{
   'update:modelValue': [value: string]
 }>()
+
+const editor = useEditor({
+  content: props.modelValue || '',
+  extensions: [StarterKit],
+  onUpdate: ({ editor: editorInstance }: { editor: Editor }) => {
+    emit('update:modelValue', editorInstance.getHTML())
+  }
+})
+
+watch(() => props.modelValue, (value) => {
+  const isSame = editor.value.getHTML() === value
+  if (isSame) return
+  editor.value.commands.setContent(value)
+})
 </script>
 
 <template>
   <div v-bind="attrs">
-    {{ modelValue }}
+    <editor-content :editor="editor" />
   </div>
 </template>
