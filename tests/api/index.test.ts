c24af3766cbc6af3b34c31bb16249068643d5a2a
Dipesh Acharya <dipesh@awecode.com>
2025-07-22 11:44:25 +0545

    chore: add test project

--- tests/api/index.test.ts
+++ tests/api/index.test.ts
@@ -1,19 +1,28 @@
+// @vitest-environment nuxt
 import { useAdminRegistry } from '#layers/autoadmin/composables/useAdminRegistry'
 import { $fetch, setup } from '@nuxt/test-utils/e2e'
 import { describe, expect, it } from 'vitest'
 
 await setup({
-  host: 'http://localhost:3000',
+  // host: 'http://localhost:3000',
   nuxtConfig: {
-    plugins: ['../helpers/plugin.ts'],
+    plugins: ['#layers/autoadmin/tests/helpers/plugin.ts'],
+    debug: true,
   },
 })
 
 describe('index page', async () => {
   it('index page', async () => {
+    console.log('index page')
     const registry = useAdminRegistry()
     console.log(registry)
-    const response = await $fetch('/admin')
-    expect(response).toBeDefined()
+    try {
+      const res = await $fetch('/admin')
+      console.log(res)
+    } catch (error: any) {
+      console.log(error.stack)
+      console.error('Fetch failed:', error?.response?._data || error)
+      throw error
+    }
   })
 })
