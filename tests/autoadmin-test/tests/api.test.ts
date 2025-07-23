805ef7ebb93765342441de9370e2611b74a823b8
Dipesh Acharya <dipesh@awecode.com>
2025-07-23 19:04:19 +0545

    test: creating a post

--- tests/autoadmin-test/tests/api.test.ts
+++ tests/autoadmin-test/tests/api.test.ts
@@ -26,6 +26,7 @@ describe('api', async () => {
       }
     }
   })
+
   it('should create 2 records for each model', async () => {
     const modelConfigs = [
       {
@@ -73,4 +74,187 @@ describe('api', async () => {
       expect(listResponse.results[1]!.name).toBe(payloads[1]!.name)
     }
   })
+
+  it('should create a post', async () => {
+    // Get formspec for posts
+    const formSpec = await $fetch('/api/autoadmin/formspec/posts')
+    expect(formSpec).toEqual({
+      "spec": {
+        "fields": [
+          {
+            "name": "title",
+            "label": "Title",
+            "type": "text",
+            "required": true,
+            "rules": {}
+          },
+          {
+            "name": "slug",
+            "label": "Slug",
+            "type": "text",
+            "required": true,
+            "rules": {}
+          },
+          {
+            "name": "excerpt",
+            "label": "Excerpt",
+            "type": "textarea",
+            "required": false,
+            "rules": {},
+            "help": "Brief summary of the post (max 200 characters)",
+            "inputAttrs": {
+              "maxlength": 200
+            }
+          },
+          {
+            "name": "content",
+            "label": "Post Content",
+            "type": "rich-text",
+            "required": false,
+            "rules": {}
+          },
+          {
+            "name": "featuredImage",
+            "label": "Featured Image",
+            "type": "image",
+            "required": false,
+            "rules": {},
+            "fileConfig": {
+              "accept": [
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".webp"
+              ],
+              "prefix": "posts/",
+              "maxSize": 5242880
+            }
+          },
+          {
+            "name": "status",
+            "label": "Status",
+            "type": "select",
+            "required": false,
+            "rules": {},
+            "options": [
+              "draft",
+              "published",
+              "archived"
+            ],
+            "defaultValue": "draft"
+          },
+          {
+            "name": "views",
+            "label": "Views",
+            "type": "number",
+            "required": false,
+            "rules": {
+              "min": -9007199254740991,
+              "max": 9007199254740991
+            },
+            "defaultValue": 0
+          },
+          {
+            "name": "isCommentsEnabled",
+            "label": "Is Comments Enabled",
+            "type": "boolean",
+            "required": false,
+            "rules": {},
+            "defaultValue": true
+          },
+          {
+            "name": "publishedAt",
+            "label": "Published At",
+            "type": "date",
+            "required": false,
+            "rules": {}
+          },
+          {
+            "name": "authorId",
+            "label": "Author",
+            "type": "relation",
+            "required": true,
+            "rules": {
+              "min": -9007199254740991,
+              "max": 9007199254740991
+            },
+            "relationConfig": {
+              "choicesEndpoint": "/api/autoadmin/formspec/posts/choices/authorId",
+              "relatedConfigKey": "users",
+              "enableCreate": true,
+              "enableUpdate": true,
+              "foreignRelatedColumnName": "id",
+              "foreignLabelColumnName": "email"
+            }
+          },
+          {
+            "name": "categoryId",
+            "label": "Category",
+            "type": "relation",
+            "required": false,
+            "rules": {
+              "min": -9007199254740991,
+              "max": 9007199254740991
+            },
+            "relationConfig": {
+              "choicesEndpoint": "/api/autoadmin/formspec/posts/choices/categoryId",
+              "relatedConfigKey": "categories",
+              "enableCreate": true,
+              "enableUpdate": true,
+              "foreignRelatedColumnName": "id",
+              "foreignLabelColumnName": "name"
+            }
+          },
+          {
+            "name": "___tags___tagId",
+            "type": "relation-many",
+            "label": "Tags",
+            "relationConfig": {
+              "choicesEndpoint": "/api/autoadmin/formspec/posts/choices-many/___tags___tagId",
+              "enableCreate": true,
+              "enableUpdate": true
+            },
+            "required": false,
+            "rules": {},
+            "options": []
+          }
+        ],
+        "warnOnUnsavedChanges": true
+      }
+    })
+
+    const authors = await $fetch<{label: string, value: number}[]>(formSpec.spec.fields.find((field: any) => field.name === 'authorId')?.relationConfig?.choicesEndpoint!)
+    const authorId = authors[0]?.value
+    expect(authorId).toBeTruthy()
+
+    const categories = await $fetch<{label: string, value: number}[]>(formSpec.spec.fields.find((field: any) => field.name === 'categoryId')?.relationConfig?.choicesEndpoint!)
+    const categoryId = categories[0]?.value
+    expect(categoryId).toBeTruthy()
+
+    const tags = await $fetch<{label: string, value: number}[]>(formSpec.spec.fields.find((field: any) => field.name === '___tags___tagId')?.relationConfig?.choicesEndpoint!)
+    const tagIds = tags.map((tag: any) => tag.value)
+    expect(tagIds).toBeTruthy()
+
+    // Create post
+    const postPayload = {
+      title: 'Post 1',
+      slug: 'post-1',
+      excerpt: 'Excerpt 1',
+      content: 'Content 1',
+      featuredImage: 'https://example.com/image.jpg',
+      publishedAt: '1991-12-15',
+      status: 'published',
+      views: 100,
+      isCommentsEnabled: false,
+      authorId,
+      categoryId,
+      tagIds,
+    }
+    const postResponse = await $fetch<{ data: { id: number } }>('/api/autoadmin/posts', {
+      method: 'POST',
+      body: postPayload,
+    })
+    expect(postResponse.data.id).toBeTruthy()
+  })
+
 })
