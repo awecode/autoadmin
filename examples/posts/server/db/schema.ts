160a33ae5403784e1f7eec653491b484dc3343a2
Dipesh Acharya <dipesh@awecode.com>
2026-03-14 17:12:04 +0545

    chore: remove toplevel await import

--- examples/posts/server/db/schema.ts
+++ examples/posts/server/db/schema.ts
@@ -1,8 +1,8 @@
 import process from 'node:process'
+import * as postgresSchema from './postgres'
+import * as sqliteSchema from './sqlite'
 
 const dialect = process.env.NUXT_DATABASE_DIALECT === 'postgres' ? 'postgres' : 'sqlite'
-const schema = dialect === 'postgres'
-  ? await import('./postgres')
-  : await import('./sqlite')
+const schema = dialect === 'postgres' ? postgresSchema : sqliteSchema
 
 export const { categories, posts, postsToTags, tags, users } = schema
