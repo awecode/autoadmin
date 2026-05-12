80fe220ccd17df2c76433057d276548947693ee7
Dipesh Acharya <dipesh@awecode.com>
2026-05-12 19:12:25 +0545

    fix: return create path in registry meta (for dashboard index) only if create is allowed

--- server/api/autoadmin/registry-meta.get.ts
+++ server/api/autoadmin/registry-meta.get.ts
@@ -1,15 +1,19 @@
 import { getIconForLabel } from '#layers/autoadmin/utils/string'
+import { getAllowedActions } from '../../utils/roleHelpers'
 
-export default defineEventHandler(async () => {
+export default defineEventHandler(async (event) => {
   const registry = useAdminRegistry()
   const allCfg = registry.all()
-  const meta = allCfg.filter(cfg => cfg.enableIndex).map(cfg => ({
-    label: cfg.label,
-    icon: cfg.icon || getIconForLabel(cfg.label),
-    to: { name: 'autoadmin-list', params: { modelKey: cfg.key } },
-    type: 'link' as const,
-    createPath: cfg.create.enabled ? cfg.create.route : undefined,
-    searchPlaceholder: cfg.list.enableSearch ? cfg.list.searchPlaceholder : undefined,
-  }))
+  const meta = allCfg.filter(cfg => cfg.enableIndex).map((cfg) => {
+    const allowed = getAllowedActions(event, { roles: cfg.roles })
+    return {
+      label: cfg.label,
+      icon: cfg.icon || getIconForLabel(cfg.label),
+      to: { name: 'autoadmin-list', params: { modelKey: cfg.key } },
+      type: 'link' as const,
+      createPath: (cfg.create.enabled && allowed.create) ? cfg.create.route : undefined,
+      searchPlaceholder: cfg.list.enableSearch ? cfg.list.searchPlaceholder : undefined,
+    }
+  })
   return meta
 })
